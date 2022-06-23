9ebfba281304862cc091bbad1e1236984cc906cf
Chad Cook <charliert13@yahoo.com>
2022-06-22 19:30:35 -0500

    added more styling to the header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,20 @@
 import React from 'react';
 
-import AaronMullens from '../../assets/images/AMLogo.png';
+import AaronMullens from '../../assets/images/logos/AMHeaderLogo.png';
+import YouTube from '../../assets/images/logos/YouTube.png';
+import Instagram from '../../assets/images/logos/Instagram.png';
+import Facebook from '../../assets/images/logos/Facebook.png';
+import Phone from '../../assets/images/logos/Phone.png';
+import Email from '../../assets/images/logos/Email.png';
+
+
+import './Header.css';
 
 export default function Header() {
 
     return(
         <header>
-            <a href="/" ><img id="Aaron Mullens Realtor" src={AaronMullens} alt="Aaron Mullens Realtor Logo with Name and Home Picture" /></a>
+            <a href="/" ><img id="Aaron Mullens Realtor" src={AaronMullens} alt="Aaron Mullens Realtor Logo" /></a>
             <nav>
                 <a href="/">Home</a>
                 <a href="/buying">Buying</a>
@@ -15,9 +23,9 @@ export default function Header() {
                 <a href="/blog">Blog</a>
             </nav>
             {/* <ul>
-                <a href='youtube.com' target="_blank" rel="noreferrer"><img src={YouTube} alt='YouTube Logo' /></a>
-                <a href='instagram.com' target="_blank" rel="noreferrer"><img src={Instagram} alt='Instagram Logo' /></a>
-                <a href='facebook.com' target="_blank" rel="noreferrer"><img src={Facebook} alt='Facebook Logo' /></a>
+                <a href='https://www.youtube.com' target="_blank" rel="noreferrer"><img src={YouTube} alt='YouTube Logo' /></a>
+                <a href='https://www.instagram.com' target="_blank" rel="noreferrer"><img src={Instagram} alt='Instagram Logo' /></a>
+                <a href='https://www.facebook.com' target="_blank" rel="noreferrer"><img src={Facebook} alt='Facebook Logo' /></a>
                 <a href='tel:512-270-9755' title='Contact Number'><img src={Phone} alt='Phone Symbol' /></a>
                 <a href='mailto:aaron@thegrooverealty.com' title='Email'><img src={Email} alt='Email Symbol' /></a>
             </ul> */}
